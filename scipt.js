539d08a4c9cb44e38f32b79ea211d66a9dbbd1d3
Kasilanka Sai Vivek <57192336+KVivek-design@users.noreply.github.com>
2019-10-30 23:27:48 +0530

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,13 @@
+
+function div_show() 
+{
+document.getElementById('abc').style.display = "block";
+}
+
+function div_hide(){
+document.getElementById('abc').style.display = "none";
+}
+function abc_()
+{
+	window.open("home.html")
+}
